ec195f8bffcbddb583d3b2a46cfe94af2e52a0f4
YaexMiko <aloksinghmains756@gmail.com>
2024-12-03 01:28:54 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -40,7 +40,7 @@ from bot.plugins.status_message_fn import (
 
 from bot.commands import Command
 from bot.plugins.call_back_button_handler import button
-sudo_users = "5179011789" 
+sudo_users = "8108281129" 
 crf.append("30")
 codec.append("libx265")
 resolution.append("854x480")
